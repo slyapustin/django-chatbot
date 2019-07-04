51401ae0178591f7d053237740a1fea29012805a
Sergey Lyapustin <s.lyapustin@gmail.com>
2019-07-04 02:18:01 +0200

    Generate multiple tasks

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from celery import group
 from .tasks import add
 
 class AddForm(forms.Form):
     a = forms.IntegerField()
     b = forms.IntegerField()
 
+    number = forms.IntegerField(label='Number of tasks', min_value=1, max_value=100000)
+
     def create_calculate_task(self):
-        return add.apply_async(args=[self.cleaned_data['a'], self.cleaned_data['b']])
+        return group(add.s(self.cleaned_data['a'], self.cleaned_data['b']) for i in range(self.cleaned_data['number']))()
--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,7 @@
+import time
 from celery import shared_task
 
 @shared_task
 def add(x, y):
+    time.sleep(0.3)
     return x + y
