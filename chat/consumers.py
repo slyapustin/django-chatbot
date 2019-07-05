8c8478e9ed02ba252b9b6779b3dacebaf7584cef
Sergey Lyapustin <s.lyapustin@gmail.com>
2019-07-06 01:22:21 +0200

    Use individual channels instead of the groups. Unfied commands. Added help and greeting.

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,53 +1,49 @@
+import json
+
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-import json
-from .tasks import add, url_status
 
+from . import tasks
+
+COMMANDS = {
+    'help': {
+        'help': 'Display help message.',
+    },
+    'sum': {
+        'args': 2,
+        'help': 'Calculate sum of two integer arguments. Example: `sum 12 32`.',
+        'task': 'add'
+    },
+    'status': {
+        'args': 1,
+        'help': 'Check website status. Example: `status twitter.com`.',
+        'task': 'url_status'
+    },
+}
 
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.group_name = 'chat'
-
-        # Join calc
-        async_to_sync(self.channel_layer.group_add)(
-            self.group_name,
-            self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        if message.startswith('sum'):
-            try:
-                _, x, y = message.split()
-                x = int(x)
-                y = int(y)
-                add.delay(x, y)
-            except:
-                pass
-        elif message.startswith('status'):
-            _, url = message.split()
-            url_status(url)
-        else:
-            message = 'Unsupported format'
-
-            # Send message to room group
-            async_to_sync(self.channel_layer.group_send)(
-                self.group_name,
+        response_message = 'Please type `help` for the list of the commands.'
+        message_parts = message.split()
+        if message_parts:
+            command = message_parts[0].lower()
+            if command == 'help':
+                response_message = 'List of the available commands:\n' + '\n'.join([f'{command} - {params["help"]} ' for command, params in COMMANDS.items()])
+            elif command in COMMANDS:
+                if len(message_parts[1:]) != COMMANDS[command]['args']:
+                    response_message = f'Wrong arguments for the command `{command}`.'
+                else:
+                    getattr(tasks, COMMANDS[command]['task']).delay(self.channel_name, *message_parts[1:])
+                    response_message = f'Command `{command}` received.'
+        
+        async_to_sync(self.channel_layer.send)(
+                self.channel_name,
                 {
                     'type': 'chat_message',
-                    'message': message
+                    'message': response_message
                 }
             )
 
@@ -56,5 +52,5 @@ class ChatConsumer(WebsocketConsumer):
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': f'bot> {message}'
+            'message': f'[bot]: {message}'
         }))
--- chat/tasks.py
+++ chat/tasks.py
@@ -9,24 +9,24 @@ from django.core.cache import cache
 channel_layer = get_channel_layer()
 
 @shared_task
-def add(x, y):
-    message = '{}+{}={}'.format(x, y, x + y)
-    async_to_sync(channel_layer.group_send)("chat", {"type": "chat.message", "message": message})
+def add(channel_name, x, y):
+    message = '{}+{}={}'.format(x, y, int(x) + int(y))
+    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
 
 
 @shared_task
-def url_status(url):
+def url_status(channel_name, url):
     if not url.startswith('http'):
         url = f'https://{url}'
 
     status = cache.get(url)
     if not status:
         try:
-            r = requests.get(url)
+            r = requests.get(url, timeout=10)
             status = r.status_code
             cache.set(url, status, 60*60)
         except requests.exceptions.RequestException:
             status = 'Not available'
 
     message = f'{url} status is {status}'
-    async_to_sync(channel_layer.group_send)("chat", {"type": "chat.message", "message": message})
+    async_to_sync(channel_layer.send)(channel_name, {"type": "chat.message", "message": message})
