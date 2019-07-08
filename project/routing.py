07bf143578ea98e875aa1c27f66b966a4dd63ac5
Sergey Lyapustin <s.lyapustin@gmail.com>
2019-07-09 01:06:03 +0200

    Updated comment

--- project/routing.py
+++ project/routing.py
@@ -3,7 +3,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    # Empty for now (http->django views is added by default)
+    # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
