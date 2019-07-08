377f784dd491152fe18397a79172fc8862cf05b0
Sergey Lyapustin <s.lyapustin@gmail.com>
2019-07-09 01:16:25 +0200

    Removed unused AuthMiddlewareStack

--- project/routing.py
+++ project/routing.py
@@ -1,12 +1,9 @@
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
+    'websocket': URLRouter(
+        chat.routing.websocket_urlpatterns
+        ),
 })
