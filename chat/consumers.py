c3fbdc8b59eb17396ca50cc4413bbc400ed338c0
Roman Yaniev <r0m4r0m4n@gmail.com>
2021-02-02 12:39:10 +0200

    Update dependencies, rewrite async backend

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,7 +1,5 @@
 import json
-
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 from . import tasks
 
@@ -21,36 +19,57 @@ COMMANDS = {
     },
 }
 
-class ChatConsumer(WebsocketConsumer):
-    def receive(self, text_data):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
         response_message = 'Please type `help` for the list of the commands.'
         message_parts = message.split()
         if message_parts:
             command = message_parts[0].lower()
             if command == 'help':
-                response_message = 'List of the available commands:\n' + '\n'.join([f'{command} - {params["help"]} ' for command, params in COMMANDS.items()])
+                response_message = 'List of the available commands:\n' + '\n'.join(
+                    [f'{command} - {params["help"]} ' for command, params in COMMANDS.items()])
             elif command in COMMANDS:
                 if len(message_parts[1:]) != COMMANDS[command]['args']:
                     response_message = f'Wrong arguments for the command `{command}`.'
                 else:
                     getattr(tasks, COMMANDS[command]['task']).delay(self.channel_name, *message_parts[1:])
                     response_message = f'Command `{command}` received.'
-        
-        async_to_sync(self.channel_layer.send)(
-                self.channel_name,
-                {
-                    'type': 'chat_message',
-                    'message': response_message
-                }
-            )
+        # Send message to room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': response_message
+            }
+        )
 
-    def chat_message(self, event):
+    # Receive message from room group
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': f'[bot]: {message}'
         }))
